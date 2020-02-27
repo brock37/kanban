2fcf2c97b46b012d7cc5c0f6dc8add08326a0458
Brock37 <remondflorent@gmail.com>
2020-02-27 15:28:51 +0100

    replace old navbar style with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -14,25 +14,13 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style lang="css">
+h1.nav-brand {
+  font-size: 24px;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+img.account {
+  width: 48px;
+  height: 48px;
 }
 </style>
