edb13be33c694ceee10ffb05608df83aa6fac76e
Brock37 <remondflorent@gmail.com>
2020-02-27 16:13:47 +0100

    Create list component

---
+++ src/components/taskList.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div class="container bg-light mx-auto mt-1 pb-2">
+    <h3 class="bg-primary text-light mx-0">{{ title }}</h3>
+    <task></task>
+    <task></task>
+    <task></task>
+
+  </div>
+
+</template>
+
+<script>
+import task from './task.vue'
+
+export default {
+  components :{
+    task
+  },
+  data(){
+    return{
+      title : "Titre de la liste"
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+h3{
+  font-size: 14px;
+}
+</style>
