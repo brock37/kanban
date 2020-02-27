baf982e8a879d07f83b30a96f7531acc18b0c9c0
Brock37 <remondflorent@gmail.com>
2020-02-27 16:13:34 +0100

    Create Task Component

---
+++ src/components/task.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="container my-2 rounded">
+    <label for="completed"> {{ text }}</label>
+    <input type="checkbox" name="completed" v-bind:value="completed">
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      text : "Ma tache a accomplir",
+      completed : false,
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.container{
+  background: #EFE6D8;
+}
+</style>
