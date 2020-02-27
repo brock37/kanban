296ff9376f3a24960ec5667b2c8b85b532e5a568
Brock37 <remondflorent@gmail.com>
2020-02-27 16:14:02 +0100

    include taskList component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="home">
-
+  <div class="home bg-secondary">
+    <taskList></taskList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import taskList from '@/components/taskList.vue'
 
 
 export default {
   name: 'Home',
   components: {
+    taskList
   }
 }
 </script>
